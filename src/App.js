05ceda64c36e78d536b701ffe000a38f90401601
viniciuslunardi <vinilunardi00@gmail.com>
2020-02-26 21:21:38 -0300

    adicionando mais conversores

--- src/App.js
+++ src/App.js
@@ -7,8 +7,19 @@ import "./component/Converter.css"
 function App() {
   return (
     <div className="App">
-    <Converter currencyA="USD" currencyB="BRL">
-    </Converter>
+      <h1>Currency converter</h1>
+      <div className="line">
+      <Converter currencyA="USD" currencyB="BRL"></Converter>
+        <Converter currencyA="BRL" currencyB="USD"></Converter>
+      </div>
+      <div className="line">
+        <Converter currencyA="CAD" currencyB="BRL"></Converter>
+        <Converter currencyA="BRL" currencyB="CAD"></Converter>
+      </div>
+      <div className="line">
+        <Converter currencyA="EUR" currencyB="BRL"></Converter>
+        <Converter currencyA="BRL" currencyB="EUR"></Converter>
+      </div>
     </div>
   );
 }
